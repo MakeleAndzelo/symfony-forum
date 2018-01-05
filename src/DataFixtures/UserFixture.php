21dc755555b33d5eea13aedf213c418743d78125
MakeleAndzelo <97.piotrowski.michal@gmail.com>
2018-01-05 19:48:16 +0100

    Added faker, added user entity and fixtures

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixture extends Fixture
+{
+    private $faker;
+
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->faker = Factory::create();
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i <= 10; $i++) {
+            $user = new User();
+            $user->setUsername($this->faker->userName);
+            $user->setEmail($this->faker->email);
+
+            $password = $this->encoder->encodePassword($user, 'admin123');
+            $user->setPassword($password);
+
+            $manager->persist($user);
+        }
+        dump($manager->);
+        $manager->flush();
+    }
+}

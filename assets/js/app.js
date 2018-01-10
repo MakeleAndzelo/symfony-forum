13d2fce5d052ed6f29c001076443ebca7c4d3ccf
MakeleAndzelo <97.piotrowski.michal@gmail.com>
2018-01-10 17:57:57 +0100

    Added prototype of vue flash message component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
 import Vue from 'vue';
+import Flash from './components/Flash';
 
 new Vue({
     el: "#root",
+    delimiters: ['${', '}'],
+    components: {
+        'flash': Flash
+    }
 });
---
+++ assets/js/components/Flash.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="alert alert-info alert-custom" v-show="isVisible">
+        {{ message }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['message', 'type'],
+
+        data() {
+            return {
+                isVisible: true,
+            }
+        },
+
+        mounted() {
+            setTimeout(() => {
+                this.isVisible = false;
+            }, 3000);
+        }
+    }
+</script>
